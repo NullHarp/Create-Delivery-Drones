6157b61a03546b2aac8c1c581ac2ec11cdeaacc3
Gavin S <gavins@obsidiancorestudios.com>
2025-03-11 16:01:39 -0400

    added stored air and getter / setter functions

--- src/main/java/com/nullharp/createdeliverydrones/entities/DeliveryDrone/DeliveryDrone.java
+++ src/main/java/com/nullharp/createdeliverydrones/entities/DeliveryDrone/DeliveryDrone.java
@@ -12,7 +12,8 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 
 public class DeliveryDrone extends Mob {
-    private static final EntityDataAccessor<Integer> DATA_EXAMPLE = SynchedEntityData.defineId(DeliveryDrone.class, EntityDataSerializers.INT);
+    private static final EntityDataAccessor<Integer> STORED_AIR = SynchedEntityData.defineId(DeliveryDrone.class, EntityDataSerializers.INT);
+    private static final int MAX_AIR = 1000;
 
     public DeliveryDrone(EntityType<? extends Mob> type, Level world) {
         super(type, world);
@@ -21,7 +22,7 @@ public class DeliveryDrone extends Mob {
     @Override
     protected void defineSynchedData() {
         super.defineSynchedData();
-        this.entityData.define(DATA_EXAMPLE, 0); // Set default value
+        this.entityData.define(STORED_AIR, 0); // Set default value
     }
 
     @Override
@@ -29,6 +30,16 @@ public class DeliveryDrone extends Mob {
         this.goalSelector.addGoal(3, new HoverGoal(this, 5.0D));
         this.goalSelector.addGoal(1, new FloatGoal(this));
         this.goalSelector.addGoal(2, new LookAtPlayerGoal(this, Player.class, 8.0F));
+    }
+
+    public void setStoredAir(int value) {
+        if (value >= MAX_AIR) {
+            entityData.set(STORED_AIR,MAX_AIR);
+        } else entityData.set(STORED_AIR, Math.max(value, 0));
+    }
+
+    public int getStoredAir() {
+        return entityData.get(STORED_AIR);
     }
 
     @Override
